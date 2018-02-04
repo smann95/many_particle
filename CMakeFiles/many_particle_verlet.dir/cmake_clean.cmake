75f9faaa86000cf9c76b110edf2cb9038cc29646
smann95 <smann95@hotmail.com>
2018-02-04 15:20:40 -0500

    CMake file and new repository

---
+++ CMakeFiles/many_particle_verlet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/many_particle_verlet.dir/many_particle_verlet.cpp.o"
+  "many_particle_verlet.pdb"
+  "many_particle_verlet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/many_particle_verlet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ many_particle_verlet.cpp
@@ -0,0 +1,8 @@
+#include "many_particle_verlet.h"
+using namespace std;
+
+int main()
+{
+cout << get_random_number () << endl;
+  return 0;
+}
