6689639b1c9f4a2300ca66601a76c7794414ef4e
smann95 <smann95@hotmail.com>
2018-02-08 12:21:28 -0500

    attempt at random number generator

--- many_particle_verlet.h
+++ many_particle_verlet.h
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <string>
 #include <random>
+#include <cstdlib>
 struct particle{
   double x[3],
   v[3],
@@ -11,12 +12,17 @@ struct particle{
   std::string type;
 };
 
+double random_range(double min, double max)
+{
+  return min + (random() / ((double)RAND_MAX)*(max-min));
+}
+
 typedef std::mt19937 rng_type;
 std::uniform_int_distribution<rng_type::result_type> udist(0, 7);
 rng_type rng;
 
 double get_random_number (){
-  rng_type::result_type const seedval = get_seed();
+  rng_type::result_type const seedval = get_seed() //I think I'm supposed to use my own function for the seed, right?  I'm not sure how to actually make this work, though.
   rng.seed(seedval);
 
   rng_type::result_type random_number = udist(rng);
