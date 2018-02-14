2b55661adb6ec9113defdc3f346e403ed746b8f0
smann95 <smann95@hotmail.com>
2018-02-14 14:55:41 -0500

    switched to taking arguments from file

--- many_particle_verlet.cpp
+++ many_particle_verlet.cpp
@@ -1,10 +1,27 @@
 #include "many_particle_verlet.h"
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
+  if (argc != 2){
+    cout << "Please provide a file of conditions." << endl;
+    return 1;
+  }
+
+vector<double> conditions;
+double temp;
+ifstream input (argv[1]);
+  while (input >> temp){
+  conditions.push_back(temp);
+} 
+
+  np = conditions[0];
+  L = conditions[1];
+  T = conditions[2];
+
   srandom(time(NULL));
   create_matter();
 
+ // double old_pe = get_pe();
   return 0;
 }
--- many_particle_verlet.h
+++ many_particle_verlet.h
@@ -7,25 +7,65 @@
 #include <fstream>
 using namespace std;
 struct particle{
-  double x[3],
-  v[3],
-  a[3],
-  mass,
-  w;
+  double x[3];
   std::string type;
 };
 
-int np = 2;
-int L = 100;
+int np;
+double L;
+double k = 1;
+double T;
 vector<particle> particles;
+
+//random number between min and max
 double get_random_number(double min, double max)
 {
   return min + (random() / ((double)RAND_MAX)*(max-min));
 }
 
 double get_distance(int a, int b, const vector<particle> & v);
-void gib_data_pls();
+void gib_data_bls();
+double get_pe();
+void create_matter();
+bool evaluate_pe();
 
+//finds potential energy 
+double get_pe(double sigma, double epsilon)
+{
+  double s = sigma,
+         s2 = s*s,
+         s6 = s2*s2*s2,
+         s12 = s6*s6;
+
+  double e = epsilon;
+  double pe = 0;
+  for (int a = 0; a < np; a++){
+    for (int b = a + 1; b < np; b++){
+      double r = get_distance(a, b, particles),
+             r2 = r*r,
+             r6 = r2*r2*r2,
+             r12 = r6*r6;
+      pe += 4*e*((s12/r12) - (s6/r6));
+    }
+  }
+  return pe;
+}
+
+//takes change in potential energy after a move to evaluate if the move
+//is good or not
+bool evaluate_pe(double old_pe, double new_pe)
+{
+  bool good;
+  double change_pe = new_pe - old_pe;
+  double prob = exp(-1*change_pe*(1/(k*T)));
+  if (prob > ((double)random()/(double)RAND_MAX)){
+    good = true;
+  }
+  else {
+    good = false;
+  }
+  return good;
+}
 
 // Sets first particle at (0, 0, 0) and the rest at random positions
 void create_matter()
@@ -59,14 +99,14 @@ void create_matter()
       }
     }
   }
-  gib_data_pls();
+  gib_data_bls();
 }
 
 //computes distance for each individual coordinate and then finds the total
 //distance
 double get_distance(int a, int b, const vector<particle> & v)
 {
-  double r;
+  double d;
   double change_x;
   double change_y;
   double change_z;
@@ -88,19 +128,19 @@ double get_distance(int a, int b, const vector<particle> & v)
   double change2_y = change_y * change_y;
   double change2_z = change_z * change_z;
 
-  r = sqrt(change2_x + change2_y + change2_z);
-  return r;
+  d = sqrt(change2_x + change2_y + change2_z);
+  return d;
 }
 
-void gib_data_pls()
+void gib_data_bls()
 {
-ofstream data;
-data.open("data.xyz");
-data << np << "\n\n";
+  ofstream data;
+  data.open("data.xyz");
+  data << np << "\n\n";
 
-for (int n = 0; n < np; n++){
-  data << "Ar " << particles[n].x[0] << " " <<
-    particles[n].x[1] << " " << particles[n].x[2] << endl;
-}
-data.close();
+  for (int n = 0; n < np; n++){
+    data << "Ar " << particles[n].x[0] << " " <<
+      particles[n].x[1] << " " << particles[n].x[2] << endl;
+  }
+  data.close();
 }
